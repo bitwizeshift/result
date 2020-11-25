0727401aa8d167230b4547cb1157bcffee09226e
Matthew Rodusek <matthew.rodusek@gmail.com>
2020-11-24 20:18:32 -0500

    🚦 Replace travis/appveyor with Github Actions
    The github actions from Backport have been copied over to allow for
    proper continuous integration feedback for C++11 support.

--- .appveyor/install.cmd
+++
@@ -1,9 +0,0 @@
-SETLOCAL EnableDelayedExpansion
-
-@REM  # Install 'conan'
-
-echo "Downloading conan..."
-set PATH=%PATH%;%PYTHON%/Scripts/
-pip.exe install conan
-conan user # Create the conan data directory
-conan --version
--- .appveyor/test.cmd
+++
@@ -1,7 +0,0 @@
-SETLOCAL EnableDelayedExpansion
-
-@REM  # Test the project
-
-cd build
-
-ctest -j 4 -C %CONFIGURATION% || exit /b !ERRORLEVEL!
